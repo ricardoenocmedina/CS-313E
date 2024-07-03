ff2e33bc8ddeac2f46ab46f8230a9a3d7a8c003c
Ricardo Medina <ricardo.medina.enoc@gmail.com>
2024-07-02 22:46:13 -0500

    Added new files to Spiral

---
+++ Spiral/spiral.py
@@ -0,0 +1,36 @@
+#  File: Spiral.py
+
+#  Description:
+
+#  Student Name: Ricardo Medina
+
+#  Student UT EID: rem3885
+
+#  Course Name: CS 313E
+
+
+# Input: n is an odd integer between 1 and 100
+# Output: returns a 2-D list representing a spiral
+#         if n is even add one to n
+def create_spiral ( n ):
+  pass
+
+# Input: spiral is a 2-D list and n is an integer
+# Output: returns an integer that is the sum of the
+#         numbers adjacent to n in the spiral
+#         if n is outside the range return 0
+def sum_adjacent_numbers (spiral, n):
+  pass
+
+def main():
+  pass
+  # read the input file
+
+  # create the spiral
+
+  # add the adjacent numbers
+
+  # print the result
+
+if __name__ == "__main__":
+  main()
