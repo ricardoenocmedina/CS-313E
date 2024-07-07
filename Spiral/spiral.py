2f4a5e9ab87dfcd2b09dcc3b7fe91095bf55ff97
Ricardo Medina <ricardo.medina.enoc@gmail.com>
2024-07-07 14:01:56 -0500

    Finished create_spiral method

--- Spiral/spiral.py
+++ Spiral/spiral.py
@@ -1,19 +1,64 @@
-#  File: Spiral.py
+"""
+File: Spiral.py
 
-#  Description: 
+Description: 
 
-#  Student Name: Ricardo Medina
+Student Name: Ricardo Medina
 
-#  Student UT EID: rem3885
+Student UT EID: Ricardo Medina
 
-#  Course Name: CS 313E
-
+Course Name: CS 313E
+"""
 
 # Input: n is an odd integer between 1 and 100
 # Output: returns a 2-D list representing a spiral
 #         if n is even add one to n
 def create_spiral ( n ):
-  pass
+  """Function to create a spiral matrix with """
+
+  mat = [[0 for x in range(n)] for x in range(n)]
+
+  row_pos = n // 2
+  col_pos = n// 2
+
+  mat[row_pos][col_pos] = 1
+
+  x = 1        # slider index
+  curr_val = 2 # current value
+
+  # iterate while slider is less than the dimensions
+  while x < n:
+    
+    # fill columns
+    for i in range(x):
+      col_pos = (col_pos + 1) if (x % 2 != 0) else (col_pos - 1)
+      mat[row_pos][col_pos] = curr_val
+      curr_val += 1
+
+  
+    # fill rows
+    for j in range(x):
+      row_pos = (row_pos + 1) if (x % 2 != 0) else (row_pos -1)
+      mat[row_pos][col_pos] = curr_val
+      curr_val += 1
+
+    x += 1
+
+  # fill last row
+  for i in range(n-1):
+    col_pos = (col_pos + 1) if (x % 2 != 0) else (col_pos - 1)
+    mat[row_pos][col_pos] = curr_val
+    curr_val += 1
+
+  return mat
+
+def print_mat(mat):
+  """Helper function to print matrix nicely"""
+  for row in mat:
+    for num in row:
+      print('{: <5}'.format(num), end='')
+    print()
+    
 
 # Input: spiral is a 2-D list and n is an integer
 # Output: returns an integer that is the sum of the
@@ -23,7 +68,8 @@ def sum_adjacent_numbers (spiral, n):
   pass
 
 def main():
-  pass
+  mat = create_spiral(11)
+  print_mat(mat)
   # read the input file
 
   # create the spiral
